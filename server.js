9bbf7e7982c503a8aaed4adba7453e5b296f0084
artur <barseghyan020483@gmail.com>
2019-12-07 17:20:27 +0400

    add array method

--- server.js
+++ server.js
@@ -3,10 +3,15 @@ const http = require('http');
 const hostname = '127.0.0.1';
 const port = 3000;
 
+console.log(1111);
+
+var fruits = ["Banana", "Orange", "Apple", "Mango"];
+
 const server = http.createServer((req, res) => {
+	console.log(22222);
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello Worldn');
+  res.setHeader('Content-Type', 'application/json');
+  res.end(JSON.stringify({aaa:45}));
 });
 
 server.listen(port, hostname, () => {
